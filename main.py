d4958a33940731548e39149c7bff46babc807692
Koki <kgoudy64@gmail.com>
2022-05-10 15:09:06 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+#Using PrettyTable to create charts in Python
+
+from prettytable import PrettyTable
+
+table = PrettyTable()
+table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
+table.add_column("Type", ["Electric", "Water", "Fire"])
+
+
+print(table)
